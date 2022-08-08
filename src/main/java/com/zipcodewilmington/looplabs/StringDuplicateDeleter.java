a0c905e785d85409983d238040252ca97a680258
Chris Kent <Chris_Kent@ymail.com>
2022-08-08 19:16:00 -0400

    Completed

--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -1,5 +1,9 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by leon on 1/28/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
@@ -11,11 +15,61 @@ public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
 
     @Override
     public String[] removeDuplicates(int maxNumberOfDuplications) {
-        return new String[0];
+        ArrayList<String> list = new ArrayList<>();
+        HashMap<String, Integer> map = new HashMap<>();
+        Integer arrayit = 0;
+        Integer count = 0;
+        for (String str : this.array) {
+            if(!map.containsKey(str)){
+                map.put(str, 1);
+            }
+            else{
+                count = map.get(str) + 1;
+                map.put(str, count);
+            }
+        }
+        for (Map.Entry<String, Integer> entry:map.entrySet()) {
+            if(entry.getValue() < maxNumberOfDuplications){
+                arrayit = entry.getValue();}
+            else{arrayit = 0;}
+            while(arrayit > 0){
+                list.add(entry.getKey());
+                arrayit--;
+            }
+
+        }
+
+
+        return list.toArray(new String[0]);
     }
 
     @Override
     public String[] removeDuplicatesExactly(int exactNumberOfDuplications) {
-        return new String[0];
-    }
+            ArrayList<String> list = new ArrayList<>();
+            HashMap<String, Integer> map = new HashMap<>();
+            Integer arrayit = 0;
+            Integer count = 0;
+            for (String str : this.array) {
+                if(!map.containsKey(str)){
+                    map.put(str, 1);
+                }
+                else{
+                    count = map.get(str) + 1;
+                    map.put(str, count);
+                }
+            }
+            for (Map.Entry<String, Integer> entry:map.entrySet()) {
+                if(entry.getValue() != exactNumberOfDuplications){
+                    arrayit = entry.getValue();}
+                else{arrayit = 0;}
+                while(arrayit > 0){
+                    list.add(entry.getKey());
+                    arrayit--;
+                }
+
+            }
+
+
+            return list.toArray(new String[0]);
+        }
 }
